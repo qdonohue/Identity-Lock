9d10f9da49a02bcbbb52e3f1ba852124c226464c
Quinn Donohue <quinndonohue@gmail.com>
2021-11-10 15:08:37 -0500

    Initial working tailwind navbar

--- identity-lock/src/navbar.js
+++ identity-lock/src/navbar.js
@@ -6,6 +6,8 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
+import logo from './FullLogo.jpg'
+
 const navigation = [
     { name: 'Home', path: '/home' },
     { name: 'Documents', path: '/documents' },
@@ -21,32 +23,17 @@ export default function NavBar() {
     const { isAuthenticated, user, loginWithPopup } = useAuth0();
     return (
         <Disclosure as="nav" className="bg-gray-800">
+        {isAuthenticated ? }
             {({ open }) => (
                 <>
                     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
                                     <img
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                                        alt="Workflow"
-                                    />
-                                    <img
                                         className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                                        alt="Workflow"
+                                        src={logo}
+                                        alt="Identity Lock"
                                     />
                                 </div>
                                 <div className="hidden sm:block sm:ml-6">
