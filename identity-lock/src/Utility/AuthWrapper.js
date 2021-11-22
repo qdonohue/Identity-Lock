faf3bc9fb8a26a93c3c862c50e69c402b4fc744e
Quinn Donohue <quinndonohue@gmail.com>
2021-11-22 14:49:39 -0500

    Loading spinner

--- identity-lock/src/Utility/AuthWrapper.js
+++ identity-lock/src/Utility/AuthWrapper.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import Loader from 'react-loader-spinner'
+
 
 export function AuthWrapper({ children }) {
   const {
     isLoading,
     error,
   } = useAuth0();
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="flex items-center justify-center h-screen" ><Loader type="ThreeDots" color="#00BFFF" height={120} width={120} /></div>;
   }
   if (error) {
     return <div>Oops... {error.message}</div>;
